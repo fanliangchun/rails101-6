a0d51e62388babb7e315ebaf564884f9f9717051
fanliangchun <jeremydo2010@gmail.com>
2016-12-25 17:04:30 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,7 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -65,7 +66,7 @@ class GroupsController < ApplicationController
     end
     redirect_to group_path(@group)
   end
-  
+
   private
 
   def find_group_and_check_permission
