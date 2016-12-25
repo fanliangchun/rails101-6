e9c42196ce27949fbf78b0be09023950fbae9e6a
fanliangchun <jeremydo2010@gmail.com>
2016-12-25 17:41:18 +0800

    implemet edit & delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,30 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+      redirect_to account_posts_path(@group), alert: "Post Deleted"
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+    if @post.update(post_params)
+      redirect_to account_posts_path(@group), notice: "Post Updated"
+    else
+      render :edit
+    end
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
